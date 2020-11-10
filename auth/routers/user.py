97b31046d67b5d4de4068f1187afb3a192d21ab9
josearangos <jose.arangos@udea.edu.co>
2020-11-10 01:54:29 -0500

    refactor enpoint returns

--- auth/routers/user.py
+++ auth/routers/user.py
@@ -3,8 +3,8 @@ from schemas import userSchema
 from database.connection import SessionLocal, engine
 from database import userCrud
 from sqlalchemy.orm import Session
-
 from typing import List
+from schemas.userSchema import (ResponseModel,ErrorResponseModel)
 
 
 from fastapi import APIRouter, Body,Depends,HTTPException
@@ -38,7 +38,7 @@ def read_users(skip: int = 0 , limit: int = 100, db:Session = Depends(get_db),to
 
 
 
-@router.post("/",response_model=userSchema.User)
+@router.post("/")
 def create_user(user: userSchema.UserCreate, db:Session = Depends(get_db)):
     
     db_user = userCrud.get_user_by_email(db,user.username)
@@ -46,13 +46,29 @@ def create_user(user: userSchema.UserCreate, db:Session = Depends(get_db)):
     if(db_user):
         raise HTTPException(status_code= 400, detail="Email already registered")
 
-    return userCrud.create_user(db=db, user=user)
+    user = userCrud.create_user(db=db, user=user)
 
+    return {"code":200,"message":"User added successfully"}
 
 @router.delete("/")
 def delete_user(user_id: int,db: Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
-    return userCrud.delete_user(db=db,user_id=user_id)
+
+    response = userCrud.delete_user(db=db,user_id=user_id)
+
+    if(response == False):
+        return {"code":400,"message":"User deleted error"}
+
+    return {"code":200,"message":"User deleted successfully"}
 
 @router.put("/")
 def update_user(user:userSchema.UserUpdate,db:Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
-    return userCrud.update_user(db=db,user_id=user.id,payload=user)
+
+    response = userCrud.update_user(db=db,user_id=user.id,payload=user)
+
+    if (response == False):
+
+        return {"code":400,"message":"User updated error"}
+    
+    return {"code":200,"message":"User updated successfully"}
+    
+    
--- auth/schemas/userSchema.py
+++ auth/schemas/userSchema.py
@@ -20,3 +20,4 @@ class User(UserBase):
 class UserUpdate(BaseModel):
     id: int
     password: str
+
